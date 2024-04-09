d79ed953ddb7aecd19d27b62f310a73c122af739
Eamon Savard <savarde@wit.edu>
2024-04-09 17:13:15 -0400

    Merge branch 'main' of https://github.com/ewsavard/comp2final into main

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,20 +1,30 @@
 package game;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.swing.Action;
+import javax.swing.JButton;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class Mainx extends Application implements Initializable{
+public class Main extends Application implements Initializable{
 	@FXML
 	//Button AnimalCategory;
+	
+	public Stage stage;
+	private Scene scene; 
+	private Parent parent_roots; 
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -23,10 +33,10 @@ public class Mainx extends Application implements Initializable{
 
 		try {
 			//BorderPane root = new BorderPane();
-
+			
 			final Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-			Scene scene = new Scene(root,400,400);
-			//scene.getStylesheets().add(getClass().getResource("Main").toExternalForm());
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("Main").toExternalForm());
 
 
 
@@ -35,10 +45,11 @@ public class Mainx extends Application implements Initializable{
 			primaryStage.setTitle("Foogle Geud!");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
+
 
 	public static void main(String[] args) {
 		launch(args);
@@ -46,42 +57,56 @@ public class Mainx extends Application implements Initializable{
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-
+		
 	}
+	
+	@FXML
+	public void switch_to_culture(ActionEvent event) throws IOException{
+		JButton culture =  new JButton("Culture");
+		Parent new_window_parent = FXMLLoader.load(getClass().getResource("CultureScreeen.fxml"));
+		culture.setAction((Action) this);
+		Scene new_window_scene = new Scene (new_window_parent);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(new_window_scene);
+		stage.show();
+	}
+
+	@FXML
+	public void switch_to_animal(ActionEvent event) throws IOException{
+		JButton Animal =  new JButton("Animal");
+		Parent new_window_parent = FXMLLoader.load(getClass().getResource("animal.fxml"));
+		Animal.setAction((Action) this);
+		Scene  new_window_scene = new Scene (new_window_parent);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(new_window_scene);
+		stage.show();
+	}
+	@FXML
+	public void switch_to_food(ActionEvent event) throws IOException{
+		JButton food =  new JButton("Food");
+		Parent new_window_parent = FXMLLoader.load(getClass().getResource("food.fxml"));
+		food.setAction((Action) this);
+		Scene  new_window_scene = new Scene (new_window_parent);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(new_window_scene);
+		stage.show();
+	}
+	@FXML
+	public void switch_to_people(ActionEvent event) throws IOException{
+		JButton people =  new JButton("People");
+		people.setAction((Action) this);
+		Parent new_window_parent = FXMLLoader.load(getClass().getResource("people.fxml"));
+		Scene  new_window_scene = new Scene (new_window_parent);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(new_window_scene);
+		stage.show();
+	}
+	
+	
+	
+	
+	
+	
 }
 
 
-//package game;
-//
-//import java.net.URL;
-//import java.util.ResourceBundle;
-//
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.fxml.Initializable;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//
-//public class Main extends Application implements Initializable {
-//
-//	@Override
-//	public void initialize(URL location, ResourceBundle resources) {
-//
-//	}
-//
-//	@Override
-//	public void start(Stage home_Page) throws Exception {
-//		Parent root = FXMLLoader.load(getClass().getResource("gameUIStart.fxml"));
-//		Scene scene = new Scene(root);
-//		home_Page.setScene(scene);
-//		home_Page.show();
-//
-//	}
-//
-//	public static void main(String[] args) {
-//		launch(args);
-//	}
-//
-//}
--- src/game/Scene_Controller_Class.java
+++ src/game/Scene_Controller_Class.java
@@ -3,18 +3,17 @@ package game;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
-<<<<<<< HEAD
-=======
+
 import javax.swing.Action;
 import javax.swing.JButton;
 
 import javafx.application.Application;
->>>>>>> branch 'main' of https://github.com/ewsavard/comp2final.git
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.fxml.FXML;
 public class Scene_Controller_Class {
 
 	
@@ -23,34 +22,33 @@ public class Scene_Controller_Class {
 	public Stage stage;
 	private Scene scene; 
 	private Parent parent_roots; 
-	private Stage stage;
-	private Scene scene;
-	private Parent parent_roots;
-
-
-
-	public void switch_to_scene1(ActionEvent event) throws IOException{
-		final FXMLLoader loader = new FXMLLoader(getClass().getResource("gameUIStart.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(parent_roots);
-		stage.setScene(scene);
-		stage.show();
-	}
-	public void switch_to_scene2(ActionEvent event) {
-		final FXMLLoader loader = new FXMLLoader(getClass().getResource("Second_scene.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(parent_roots);
-		stage.setScene(scene);
-		stage.show();
-
-	}
-	public void switch_to_scene3(ActionEvent event) throws IOException	{
-		final FXMLLoader loader = new FXMLLoader(getClass().getResource("ThirdScene.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(parent_roots);
-		stage.setScene(scene);
 	
+
+
+
+//	public void switch_to_scene1(ActionEvent event) throws IOException{
+//		final FXMLLoader loader = new FXMLLoader(getClass().getResource("gameUIStart.fxml"));
+//		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//		scene = new Scene(parent_roots);
+//		stage.setScene(scene);
+//		stage.show();
+//	}
+//	public void switch_to_scene2(ActionEvent event) {
+//		final FXMLLoader loader = new FXMLLoader(getClass().getResource("Second_scene.fxml"));
+//		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//		scene = new Scene(parent_roots);
+//		stage.setScene(scene);
+//		stage.show();
+//
+//	}
+//	public void switch_to_scene3(ActionEvent event) throws IOException	{
+//		final FXMLLoader loader = new FXMLLoader(getClass().getResource("ThirdScene.fxml"));
+//		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//		scene = new Scene(parent_roots);
+//		stage.setScene(scene);
+//	}
 	
+	@FXML
 	public void switch_to_culture(ActionEvent event) throws IOException{
 		JButton culture =  new JButton("Culture");
 		Parent new_window_parent = FXMLLoader.load(getClass().getResource("CultureScreeen.fxml"));
@@ -68,7 +66,7 @@ public class Scene_Controller_Class {
 
 
 
-	
+	@FXML
 	public void switch_to_animal(ActionEvent event) throws IOException{
 		JButton Animal =  new JButton("Animal");
 		Parent new_window_parent = FXMLLoader.load(getClass().getResource("animal.fxml"));
@@ -78,6 +76,7 @@ public class Scene_Controller_Class {
 		window.setScene(new_window_scene);
 		stage.show();
 	}
+	@FXML
 	public void switch_to_food(ActionEvent event) throws IOException{
 		JButton food =  new JButton("Food");
 		Parent new_window_parent = FXMLLoader.load(getClass().getResource("food.fxml"));
@@ -87,6 +86,7 @@ public class Scene_Controller_Class {
 		window.setScene(new_window_scene);
 		stage.show();
 	}
+	@FXML
 	public void switch_to_people(ActionEvent event) throws IOException{
 		JButton people =  new JButton("People");
 		people.setAction((Action) this);
